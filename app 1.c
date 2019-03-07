2c3bd248e91ecc51d3b11dc3fa544cba26efb975
gearle01 <47864743+gearle01@users.noreply.github.com>
2019-03-07 15:58:41 -0300

    Add files via upload

---
+++ app 1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+ void main(){
+ 	
+ 	int L1;
+ 	int L2;
+ 	int L3;
+ 	
+ 	printf("Digite o ano atual: %d\n");
+ 	scanf("%d",&L1);
+ 	printf("Digite o ano de nascimento: %d\n");
+ 	scanf("%d",&L2);
+    L2 = (L1 - L2);
+ 	
+ 	printf("Sua idade é: %d");
+ 	
+ 	
+ }
